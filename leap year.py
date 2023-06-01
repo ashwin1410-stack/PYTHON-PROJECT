13a1555c58364e74bfff221ebc85513d2fdee872
Ashwin Savane <110716420+ashwin1410-stack@users.noreply.github.com>
2023-06-01 19:17:14 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,16 @@
+def leap_year():
+ year = int(input("Enter the year :"))
+
+ if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        print("leap year")
+      else:
+         print("not leap year")
+    else:
+       print("not a learp year")
+ else:
+   print("not a leap year")
+
+leap_year()
+
